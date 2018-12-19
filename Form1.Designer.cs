e3f5f58d71f4875f4cac769092fa1da1ec27ce15
donlemefo <sixoperations@gmail.com>
2018-12-20 00:07:53 +0300

    mm

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace XML5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 423);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "group";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "marks";
+            this.Column4.Name = "Column4";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(511, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(477, 423);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(532, 441);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(XML5.Program);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 512);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace XML5
+{
+
+    public partial class Form1 : Form
+    {
+        List<Student> students;
+        float sum =0;
+        float c = 0,sum1;
+        float sred = 0;
+        int l = 0;
+        int dva = 0, tri=0, chet = 0, pit = 0;                                    
+                                    
+        public Form1()
+        {
+            
+            students = new List<Student>();
+            InitializeComponent();
+        }
+        public class Student
+        {
+        
+            public string name;
+            public int id;
+            public string group;
+            public List<int> marks;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Files XML (*.xml)|*.xml";
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+            string filename =dlg.FileName;
+           
+
+            try
+            {
+                XmlTextReader reader = new XmlTextReader(filename);
+                Student st=null;
+                //DataTableReader.WhilespaceHandling = WhitespaceHandling.None;
+                bool mstart = false;
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name == "stud")
+                        {
+                           
+                            st = new Student();
+                            st.marks = new List<int>();
+                            st.name = reader.GetAttribute("name");
+                            st.id = Convert.ToInt32(reader.GetAttribute("id"));
+                            st.group = reader.GetAttribute("group");
+                        }
+                        else if (reader.Name == "marks")
+                        {
+                            mstart = true;
+                        }
+                        else if (reader.Name == "mark" && mstart)
+                        {
+                            st.marks.Add(Convert.ToInt32(reader.GetAttribute("value")));                           
+                            int i = st.marks.Count;
+                            c++;
+                                                          
+                            
+                        }
+                    }
+                    else if (reader.NodeType == XmlNodeType.EndElement)
+                    {
+                        if (reader.Name == "marks")
+                        {
+                            students.Add(st);
+                            mstart = false;
+                            int i = st.marks.Count;
+                            int n = 0;
+                            for (int j = 0; j < i; j++)
+                            {
+                                if (st.marks[j] == 2)
+                                    dva++;
+                                if (st.marks[j] == 3)
+                                    tri++;
+                                if (st.marks[j] == 4)
+                                    chet++;
+                                if (st.marks[j] == 5)
+                                    pit++;
+                            }
+                            for (int j = 0; j < i; j++)
+                            {
+                                sum = sum + st.marks[j];
+                            }
+                            dataGridView1.Rows.Add(st.id.ToString(), st.name, st.group.ToString(),st.marks[n].ToString()) ;
+                            for(n++;n<i;n++)
+                            {
+                                dataGridView1.Rows.Add("","","", st.marks[n].ToString());
+                            }
+                            
+
+
+                        }
+                    }   
+                }
+                sred = sum / c;
+                textBox3.Text = sred.ToString();               
+                chart1.Series[0].Points.AddXY(2,dva);
+                    chart1.Series[0].Points.AddXY(3, tri);
+                    chart1.Series[0].Points.AddXY(4, chet);
+                    chart1.Series[0].Points.AddXY(5, pit);
+                
+            sum1 = 0;
+
+
+
+
+        }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+
+        }
+
+    
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+        }
+
+      
+    }
+    
+
+
+}
+
+   
+        
